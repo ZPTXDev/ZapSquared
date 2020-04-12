5cc46700f629c7646a8790ca547423dbb3883cc0
zapteryx <max@zapteryx.com>
2020-04-12 14:55:32 +0800

    commonly used functions

--- main.js
+++ main.js
@@ -8,6 +8,51 @@ var modulesArr = {};
 var cooldowns = {};
 var ready = false;
 
+function roundTo(n, digits) {
+    var negative = false;
+    if (digits === undefined) {digits = 0;}
+    if (n < 0) {negative = true; n = n * -1;}
+    var multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    n = (Math.round(n) / multiplicator).toFixed(digits);
+    if (negative) {n = (n * -1).toFixed(digits);}
+    if (digits == 0) {n = parseInt(n, 10);}
+    return n;
+}
+function msToTime(ms) {
+  days = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  preseconds = ms/1000;
+  while (preseconds > 59) {minutes = minutes+1; preseconds = preseconds-60;}
+  if (preseconds < 60) {
+    while (minutes > 59) {hours = hours+1; minutes = minutes-60;}
+    if (minutes < 60) {
+      while (hours > 23) {days = days+1; hours = hours-24;}
+      if (hours < 24) {
+        seconds = roundTo(preseconds, 0);
+        if (days == 1) {daysString = "day";}
+        else {daysString = "days";}
+        if (hours == 1) {hoursString = "hr";}
+        else {hoursString = "hrs";}
+        if (minutes == 1) {minutesString = "min";}
+        else {minutesString = "mins";}
+        if (seconds == 1) {secondsString = "sec";}
+        else {secondsString = "secs";}
+        array = [];
+        if (days > 0) {array.push(days + " " + daysString + ",");}
+        if (hours > 0) {array.push(hours + " " + hoursString + ",");}
+        if (minutes > 0) {array.push(minutes + " " + minutesString + ",");}
+        if (seconds > 0) {array.push(seconds + " " + secondsString + ",");}
+        final = array.join(" ");
+        if (final == "") {return "<1 sec"}
+        return final.slice(0, -1);
+      }
+    }
+  }
+}
+
 initialTime = new Date().getTime();
 console.log("");
 console.log("        ,/");
